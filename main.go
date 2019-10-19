672d6362b58ccf544847d8de312f41027fc57cee
souvikmaji <souvikmaji94@gmail.com>
2019-10-19 10:48:40 +0530

    setup router refactored

--- main.go
+++ main.go
@@ -16,24 +16,30 @@ const (
 	indexFile = "index.html"
 )
 
-func main() {
-	port := flag.Int("port", 8000, "port number for the http server")
-	flag.Parse()
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	// health check endpoint
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
 
+func setupRouter() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		// health check endpoint
-		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-	})
+	router.HandleFunc("/api/health", healthCheck)
 
 	spa := handler{staticPath: staticDir, indexPath: indexFile}
 	router.PathPrefix("/").Handler(spa)
 
+	return router
+}
+
+func main() {
+	port := flag.Int("port", 8000, "port number for the http server")
+	flag.Parse()
+
 	listenAddr := fmt.Sprintf("127.0.0.1:%d", *port)
 
 	srv := &http.Server{
-		Handler:      router,
+		Handler:      setupRouter(),
 		Addr:         listenAddr,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
