ad3a7c52424ec2f66c0981ad65c33f788e978377
souvikmaji <souvikmaji94@gmail.com>
2019-10-19 10:35:54 +0530

    reading port number from flag

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -9,23 +11,37 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const (
+	staticDir = "static"
+	indexFile = "index.html"
+)
+
 func main() {
+	port := flag.Int("port", 8000, "port number for the http server")
+	flag.Parse()
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		// an example API handler
+		// health check endpoint
 		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 	})
 
-	spa := handler{staticPath: "static", indexPath: "index.html"}
+	spa := handler{staticPath: staticDir, indexPath: indexFile}
 	router.PathPrefix("/").Handler(spa)
+
+	listenAddr := fmt.Sprintf("127.0.0.1:%d", *port)
 
 	srv := &http.Server{
 		Handler:      router,
-		Addr:         "127.0.0.1:8000",
+		Addr:         listenAddr,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	log.Println("Server is ready to handle requests at", listenAddr)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
+	}
+
 }
