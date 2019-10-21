44057eda9b59b1dc9b0308c0aaff235baa912eb1
souvikmaji <souvikmaji94@gmail.com>
2019-10-21 08:23:29 +0530

    comments added

--- models/db.go
+++ models/db.go
@@ -9,18 +9,24 @@ type Datastore interface {
 	AllTeams(int64) ([]*Team, int64, error)
 }
 
+// DB is the wrapper for gorm db object
 type DB struct {
 	*gorm.DB
 }
 
 // NewDB creates a new database connection
 func NewDB(dataSourceName string) (*DB, error) {
+	// open new daatabase connection
 	db, err := gorm.Open("postgres", dataSourceName)
 	if err != nil {
 		return nil, err
 	}
+
+	// check if connection is properly established
 	if err = db.DB().Ping(); err != nil {
 		return nil, err
 	}
+
+	// return db wrapper
 	return &DB{db}, nil
 }
