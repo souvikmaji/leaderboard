46874cc6654cbceb904211765ac42b804189bb55
souvikmaji <souvikmaji94@gmail.com>
2020-06-09 22:13:14 +0530

    fixed empty error response bug

--- controller/response.go
+++ controller/response.go
@@ -21,20 +21,23 @@ func sendResponse(w http.ResponseWriter, response interface{}) error {
 
 // ErrorResponse represents http error response structure required by datatable api
 type errorResponse struct {
-	error string // Optional: If an error occurs during the running of the server-side processing
+	Error string // Optional: If an error occurs during the running of the server-side processing
 }
 
 // newErrorResponse creates a new error response to be consumed by the databale api
 func newErrorResponse(err error) errorResponse {
-	return errorResponse{error: err.Error()}
+	return errorResponse{Error: err.Error()}
 }
 
 func sendError(w http.ResponseWriter, errMsg error) {
 	w.Header().Set("Content-Type", "application/json")
 	log.Println("Error: ", errMsg)
+
 	response := newErrorResponse(errMsg)
+
 	e, err := json.Marshal(response)
 	if err != nil {
+		log.Println("Error marshaling json:", errMsg)
 		w.Write([]byte(err.Error()))
 	}
 
